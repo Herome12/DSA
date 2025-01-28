efb745714ea325eea3c7c147b231792794d820e8
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2025-01-28 15:02:19 +0530

    Time: 11 ms (27.26%) | Memory: 13.1 MB (50.42%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int sum = 0;
+        unordered_map<char,int>n;
+        int sum =0;
+        n={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
 
-        // Initialize Roman numeral characters
-        char I = 'I', V = 'V', X = 'X', L = 'L', C = 'C', D = 'D', M = 'M';
-
-        for (int i = 0; i < s.size(); ++i) {
-            char str = s[i];
-            
-            // Check for subtractive cases
-            if (i + 1 < s.size() && 
-                ((str == I && (s[i + 1] == V || s[i + 1] == X)) ||
-                 (str == X && (s[i + 1] == L || s[i + 1] == C)) ||
-                 (str == C && (s[i + 1] == D || s[i + 1] == M)))) {
-                // Subtract current value
-                if (str == I) sum -= 1;
-                else if (str == X) sum -= 10;
-                else if (str == C) sum -= 100;
-            } else {
-                // Add current value
-                if (str == I) sum += 1;
-                else if (str == V) sum += 5;
-                else if (str == X) sum += 10;
-                else if (str == L) sum += 50;
-                else if (str == C) sum += 100;
-                else if (str == D) sum += 500;
-                else if (str == M) sum += 1000;
+        for(int i=0;i<s.size();i++){
+            if(i+1 <s.size() && n[s[i]]<n[s[i+1]]){
+               sum -=n[s[i]];
+            }else{
+                sum +=n[s[i]];
             }
         }
         return sum;
