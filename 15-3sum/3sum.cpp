0afe0ec847859435f5746e4de9d031d5a158c805
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2025-03-08 11:58:48 +0530

    Time: 51 ms (55.47%) | Memory: 29 MB (66.67%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,12 +4,12 @@ public:
         vector<vector<int>> ans2;
         int target = 0;
 
-        sort(nums.begin(), nums.end()); // Sorting helps in duplicate removal
+        sort(nums.begin(), nums.end()); 
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
 
-            int left = i + 1, right = nums.size() - 1; // Two-pointer approach
+            int left = i + 1, right = nums.size() - 1; 
 
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
@@ -17,7 +17,7 @@ public:
                 if (sum == target) {
                     ans2.push_back({nums[i], nums[left], nums[right]});
 
-                    // Move `left` and `right` to avoid duplicates
+                    
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
