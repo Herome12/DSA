80b7bce7becca047e58eb71d36c81c90017f790c
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2025-03-17 09:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (23.53%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -12,22 +12,16 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return helper(nums, 0, nums.size() - 1);
+        return BSTarray(nums,0,nums.size()-1);
     }
 
 private:
-    TreeNode* helper(vector<int>& nums, int left, int right) {
-        if (left > right) {
-            return nullptr; // Base case: no elements to form a subtree
-        }
-        
-        int mid = left + (right - left) / 2; // Find the middle index
-        TreeNode* root = new TreeNode(nums[mid]); // Create the root node with the middle element
-
-        // Recursively build the left and right subtrees
-        root->left = helper(nums, left, mid - 1);
-        root->right = helper(nums, mid + 1, right);
-
-        return root; // Return the root node
-    }
+   TreeNode*BSTarray(vector<int>&nums,int left,int right){
+    if(left>right) return NULL;
+    int mid = left +(right-left)/2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = BSTarray(nums,left,mid-1);
+    root->right = BSTarray(nums,mid+1,right);
+    return root;
+   }    
 };
