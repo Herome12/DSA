f1eef3aff7a3a522dc480e8849b426ce027f62c4
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-01-21 10:15:37 +0530

    Time: 2 ms (68.68%) | Memory: 55.4 MB (52.40%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    const map = new Map(); 
+
+    for (let i = 0; i < nums.length; i++) {
+        const needed = target - nums[i];
+
+        if (map.has(needed)) {
+            return [map.get(needed), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
