98474c563ae51ce4e0c3190f0bf7524085ebf18d
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-01-22 15:11:36 +0530

    Time: 49 ms (5.05%) | Memory: 65.2 MB (5.10%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -1,18 +1,22 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
-var topKFrequent = function(nums, k) {
-    let size = nums.length;
-    const freqMap = new Map;
-
-    for(let num of nums){
-        freqMap.set(num,(freqMap.get(num)||0)+1);
+function topKFrequent(nums, k) {
+    // 1. Count frequencies
+    const freqMap = new Map();
+    for (let num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
     }
 
-    return [...freqMap.entries()]
-        .sort((a,b)=>b[1]-a[1])
-        .slice(0,k)
-        .map(entry =>entry[0]);
-};
+    // 2. Use a "heap" (array + sort) to keep top k
+    const heap = [];
+    for (let [num, freq] of freqMap.entries()) {
+        heap.push([freq, num]);
+        // Sort ascending by frequency
+        heap.sort((a, b) => a[0] - b[0]);
+        if (heap.length > k) heap.shift(); // remove smallest freq
+    }
+
+    // 3. Extract numbers
+    return heap.map(pair => pair[1]);
+}
+
+// Example
+console.log(topKFrequent([1,1,1,2,2,3], 2)); // [1,2]
