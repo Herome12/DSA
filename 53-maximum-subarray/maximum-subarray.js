5efaae6ae31afb67b75d4cfa029218c4ba89b86e
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-01-29 16:58:41 +0530

    Time: 2 ms (69.89%) | Memory: 64 MB (53.86%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let result = nums[0];
+
+    let sum=0;
+
+    if(nums.length == 1) return nums[0];
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        result = Math.max(result,sum);
+        if(sum<0)  sum=0;
+
+        
+    }
+    return result;
+};
