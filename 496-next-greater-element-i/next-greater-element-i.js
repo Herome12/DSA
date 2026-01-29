cfd14d2e2b6ca25cb6c380fc87812cbc5ef0240b
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-01-29 11:18:02 +0530

    Time: 4 ms (49.91%) | Memory: 55 MB (97.08%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let result = [];
+
+    for(let i=0;i<nums1.length;i++){
+        let index = nums2.indexOf(nums1[i]);
+
+        let found = -1;
+
+        for(let j = index+1;j<nums2.length;j++){
+            if(nums2[j]>nums1[i]){
+                found = nums2[j];
+                
+                break;
+            }
+             
+
+         
+        }
+
+       
+        result.push(found);
+    }
+    
+        return result;
+};
