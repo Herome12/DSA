0944c06e2d558578da148d21567e00a62f720392
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-02-19 10:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (80.77%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    nums.sort((a,b)=>a-b);
+    
+    let left =0;
+    let right = nums.length - 1;
 
-    return nums[0];
+    while(left<right){
+        let mid = Math.floor((left+right)/2);
+
+        if(nums[mid]>nums[right]){
+            left = mid+1;
+        }
+
+        else{
+            right = mid;
+        }
+    }
+
+    return nums[right];
 };
