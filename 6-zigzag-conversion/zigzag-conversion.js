9cf2976a7d4ed36471cc9238450998eca773f6ce
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-03-09 16:58:34 +0530

    Time: 7 ms (47.59%) | Memory: 60.2 MB (41.30%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+function convert(s, numRows) {
+
+    if(numRows === 1) return s;
+
+    let rows = new Array(numRows).fill("").map(() => "");
+    let currentRow = 0;
+    let goingDown = false;
+
+    for(let char of s){
+
+        rows[currentRow] += char;
+
+        if(currentRow === 0 || currentRow === numRows - 1){
+            goingDown = !goingDown;
+        }
+
+        currentRow += goingDown ? 1 : -1;
+    }
+
+    return rows.join("");
+}
