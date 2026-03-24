1aa44bbcee7b4f056ad9f0372d80780d27d08abd
Ujjwal Pal <112095501+Herome12@users.noreply.github.com>
2026-03-24 18:03:46 +0530

    Time: 125 ms (70.77%) | Memory: 84.7 MB (58.85%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s) {
+    let n = s.length;
+    let dp =  Array.from({length:n},()=>Array(n).fill(0));
+
+    for(let i=0;i<n;i++){
+        dp[i][i] = 1;
+    }
+
+    for(let i = n-1;i>=0;i--){
+        for(let j = i+1;j<n;j++){
+            if(s[i]===s[j]){
+                dp[i][j] = 2+ dp[i+1][j-1];
+            }
+            else{
+                dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1])
+            }
+
+
+        }
+    }
+
+    return dp[0][n-1]
+};
